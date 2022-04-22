0aa9f428bbdcd3c8eb1f15eb637e437337cd06b7
Margarita Poole <margaritapoole311@gmail.com>
2022-04-22 17:48:55 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Margarita from './Assets/margarita.jpg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='cardContainer' >
+        <div className='card-header'>
+          <img src={Margarita} alt='profile_picture' width='400px' height='300px'></img>
+        </div>
+        <main>
+          <header>
+            <h3>Margarita Poole</h3>
+            <h4>Front End React Developer</h4>
+            <p>portfolio site link goes here</p>
+          </header>
+          <div>
+            <button type='button'>Email</button>
+            <button type='button'>LinkedIn</button>
+          </div>
+        </main>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
