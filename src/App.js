6d02e9fd495460df7e7f12412bb18dfe538502d1
Margarita Poole <margaritapoole311@gmail.com>
2022-04-22 18:11:40 -0500

    created basic layout

--- src/App.js
+++ src/App.js
@@ -8,17 +8,26 @@ function App() {
         <div className='card-header'>
           <img src={Margarita} alt='profile_picture' width='400px' height='300px'></img>
         </div>
-        <main>
+        <main className='mainInfo'>
           <header>
-            <h3>Margarita Poole</h3>
-            <h4>Front End React Developer</h4>
+            <h1>Margarita Poole</h1>
+            <h3>Front End React Developer</h3>
             <p>portfolio site link goes here</p>
           </header>
           <div>
             <button type='button'>Email</button>
             <button type='button'>LinkedIn</button>
           </div>
+          <div>
+            <title>About</title>
+            <p>about me info goes here</p>
+            <title>Interests</title>
+            <p>fun facts goes here</p>
+          </div>
         </main>
+        <footer className='footer'>
+          social tags go here
+        </footer>
       </div>
     </div>
   )
