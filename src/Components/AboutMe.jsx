d9637e5147de67b8a8abdc31537befa6e7337109
Margarita Poole <margaritapoole311@gmail.com>
2022-04-22 22:41:08 -0500

    Removed unused file. Updated about me description

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -4,8 +4,8 @@ const AboutMe = () => {
       <span>About</span>
       <p>
         I am a frontend developer with a particular interest in making things
-        simple and automating daily tasks. I try to keep up with security and
-        best practices, and am always looking for new things to learn.
+        simple and automating daily tasks. I try to keep up with security, accessibility and
+        best practices. I am always looking for new things to learn.
       </p>
       <span>Interests</span>
       <p>
